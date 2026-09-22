4cccbba116006619ec69d64399e8a4a16d111056
NISHA PATWAL <nisha.patwal2006@gmail.com>
2026-09-22 22:28:37 +0530

    Day 2: Add array traversal problems

---
+++ Array/Travelsal/SecondLargestElementArray.java
@@ -0,0 +1,22 @@
+package Array.Travelsal;
+public class SecondLargestElementArray {
+    public static void main(String[] args) {
+
+        int[] arr = {10, 5, 8, 20, 15};
+
+        int largest = arr[0];
+        int second = Integer.MIN_VALUE;
+
+        for (int i = 1; i < arr.length; i++) {
+
+            if (arr[i] > largest) {
+                second = largest;
+                largest = arr[i];
+            } 
+            else if (arr[i] > second && arr[i] != largest) {
+                second = arr[i];
+            }
+        }
+        System.out.println("Second largest: " + second);
+    }
+}
